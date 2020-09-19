843be23510b0275ba5d3baa9c30e608c14c22e13
Viacheslav Panasovets <pansslava@gmail.com>
2020-09-19 11:58:34 +0300

    Updates

---
+++ precache-manifest.30ef35f0ab9668b0a67ca429ffff4df6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3989e98b33d0e06ba194379b4975b8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfb9a4ba4f1fd446657",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f74484445b2cf27c45aa",
+    "url": "/static/css/main.1f1d87af.chunk.css"
+  },
+  {
+    "revision": "bcfb9a4ba4f1fd446657",
+    "url": "/static/js/2.f7656b0a.chunk.js"
+  },
+  {
+    "revision": "0db23f52559317cbe1954f988324c12b",
+    "url": "/static/js/2.f7656b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74484445b2cf27c45aa",
+    "url": "/static/js/main.e00d6d5e.chunk.js"
+  },
+  {
+    "revision": "e85ec53ab238d84c6cfd",
+    "url": "/static/js/runtime-main.5d878762.js"
+  },
+  {
+    "revision": "1b24b6df6e6afbb58cefdc9950de2e34",
+    "url": "/static/media/Avatar.1b24b6df.png"
+  },
+  {
+    "revision": "0a2a0d62ae2c87b049e340701785cf89",
+    "url": "/static/media/Mapsicle Map.0a2a0d62.png"
+  },
+  {
+    "revision": "79113ba3008dc9c2b34c8c839176675e",
+    "url": "/static/media/Rectangle 7.79113ba3.png"
+  }
+]);
